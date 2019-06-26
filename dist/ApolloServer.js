7ac7b215054aa7ee658261d00c461d68e02a53bd
Valmir Barbosa <valmir.php@gmail.com>
2019-06-26 02:56:38 -0300

    chore(build): build dist

--- dist/ApolloServer.js
+++ dist/ApolloServer.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -14,8 +22,13 @@ class ApolloServer extends apollo_server_core_1.ApolloServerBase {
     // This translates the arguments from the middleware into graphQL options It
     // provides typings for the integration specific behavior, ideally this would
     // be propagated with a generic to the super class
-    async createGraphQLServerOptions(ctx) {
-        return super.graphQLServerOptions({ ctx });
+    createGraphQLServerOptions(ctx) {
+        const _super = Object.create(null, {
+            graphQLServerOptions: { get: () => super.graphQLServerOptions }
+        });
+        return __awaiter(this, void 0, void 0, function* () {
+            return _super.graphQLServerOptions.call(this, { ctx });
+        });
     }
     supportsSubscriptions() {
         return true;
@@ -30,8 +43,8 @@ class ApolloServer extends apollo_server_core_1.ApolloServerBase {
             router = Manager_1.default;
         const promiseWillStart = this.willStart();
         if (!disableHealthCheck) {
-            router.route('/.well-known/apollo/server-health', async (ctx) => {
-                await promiseWillStart;
+            router.route('/.well-known/apollo/server-health', (ctx) => __awaiter(this, void 0, void 0, function* () {
+                yield promiseWillStart;
                 // Response follows https://tools.ietf.org/html/draft-inadarei-api-health-check-01
                 ctx.response.type('application/health+json');
                 if (onHealthCheck) {
@@ -46,14 +59,14 @@ class ApolloServer extends apollo_server_core_1.ApolloServerBase {
                 else {
                     ctx.response.send({ status: 'pass' });
                 }
-            }, ['GET', 'POST']);
+            }), ['GET', 'POST']);
         }
         this.graphqlPath = path;
-        router.any(path, async (ctx) => {
-            await promiseWillStart;
+        router.any(path, (ctx) => __awaiter(this, void 0, void 0, function* () {
+            yield promiseWillStart;
             if (this.uploadsConfig && ctx.request.is(['multipart/form-data'])) {
                 try {
-                    ctx.request._body = await apollo_upload_server_1.processRequest(ctx.request.request, this.uploadsConfig);
+                    ctx.request._body = yield apollo_upload_server_1.processRequest(ctx.request.request, this.uploadsConfig);
                 }
                 catch (error) {
                     if (error.status && error.expose) {
@@ -80,7 +93,7 @@ class ApolloServer extends apollo_server_core_1.ApolloServerBase {
             return adonisApollo_1.graphqlAdonis(() => {
                 return this.createGraphQLServerOptions(ctx);
             })(ctx);
-        }, ['GET', 'POST']);
+        }), ['GET', 'POST']);
     }
 }
 exports.ApolloServer = ApolloServer;
